1b64339e91ed097994f4c4642833ef1d1816ee74
ea0723 <elenanewton@icloud.com>
2013-05-28 13:44:58 -0700

    add remember_token, initial tests, and initial sign in form

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe "Authentication" do
+  subject { page }
+  let(:submit){ click_button "Sign In" }
+
+  describe "signin" do
+    before { visit signin_path }
+
+    it { should have_content('Sign In') }
+    it { should have_title('Sign In') }
+
+    describe "with invalid information" do
+      before { click_button "Sign In" }
+
+      it { should have_title('Sign In') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error', text: 'Invalid')}
+      end
+    end
+
+    describe "with valid sign in" do
+      let(:user){FactoryGirl.create(:user)}
+      before {
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign In"
+      }
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Sign Out',    href: signout_path) }
+      it { should_not have_link('Sign In', href: signin_path) }
+    end
+
+  end
+
+end
